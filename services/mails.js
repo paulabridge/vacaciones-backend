23f622b3936146829590aa314b8620768c27eeab
paulabridge <paulabridgev@gmail.com>
2026-06-13 20:12:01 -0300

    Add files via upload

--- services/mails.js
+++ services/mails.js
@@ -2,8 +2,8 @@ const nodemailer = require('nodemailer');
 
 const transporter = nodemailer.createTransport({
   host: 'smtp.resend.com',
-  port: 465,
-  secure: true,
+  port: 587,
+  secure: false,
   auth: {
     user: 'resend',
     pass: process.env.RESEND_API_KEY,
