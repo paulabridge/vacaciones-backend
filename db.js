8c3e849db0c6e4038dafe5ba29cff5e982bcc9c3
paulabridge <paulabridgev@gmail.com>
2026-07-30 13:22:14 -0300

    Update db.js

--- db.js
+++ db.js
@@ -1,8 +1,10 @@
 const { Pool } = require('pg');
+const dns = require('dns');
+dns.setDefaultResultOrder('ipv4first');
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
-  ssl: process.env.NODE_ENV === 'production' ? { rejectUnauthorized: false } : false
+  ssl: { rejectUnauthorized: false }
 });
 
 module.exports = pool;
