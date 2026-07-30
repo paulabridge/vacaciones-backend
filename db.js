a61bc2922b2b3c07c766b2f09a6b7264692ea660
paulabridge <paulabridgev@gmail.com>
2026-07-30 13:23:35 -0300

    Update db.js

--- db.js
+++ db.js
@@ -1,10 +1,26 @@
 const { Pool } = require('pg');
-const dns = require('dns');
-dns.setDefaultResultOrder('ipv4first');
+const dns = require('dns').promises;
 
-const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
-  ssl: { rejectUnauthorized: false }
-});
+let pool;
 
-module.exports = pool;
+async function getPool() {
+  if (pool) return pool;
+  
+  const url = new URL(process.env.DATABASE_URL);
+  const hostname = url.hostname;
+  
+  // Resolver hostname a IPv4 explícitamente
+  const addresses = await dns.resolve4(hostname);
+  const ipv4 = addresses[0];
+  
+  const connectionString = process.env.DATABASE_URL.replace(hostname, ipv4);
+  
+  pool = new Pool({
+    connectionString,
+    ssl: { rejectUnauthorized: false }
+  });
+  
+  return pool;
+}
+
+module.exports = { query: async (...args) => (await getPool()).query(...args) };
